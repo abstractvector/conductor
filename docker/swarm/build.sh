afb499f806648b6dccec8bcb0e310353cb23c61c
Matt Knight <matthew.knight@gmail.com>
2018-11-01 17:38:28 -0600

    Added MySQL and InfluxDB

--- docker/swarm/build.sh
+++ docker/swarm/build.sh
@@ -6,3 +6,5 @@ TAG=latest
 docker build -t $REGISTRY_URL/mosquitto:$TAG mosquitto && docker push $REGISTRY_URL/mosquitto:$TAG
 
 docker build -t $REGISTRY_URL/node-red:$TAG node-red && docker push $REGISTRY_URL/node-red:$TAG
+
+docker build -t $REGISTRY_URL/mariadb:$TAG mariadb && docker push $REGISTRY_URL/mariadb:$TAG
