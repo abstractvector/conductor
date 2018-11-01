1e47f38d9b88afd91247c79577fce8ff75e735b6
Matt Knight <matthew.knight@gmail.com>
2018-11-01 16:37:47 -0600

    Removing --resolve-image=never from stack deploy

--- docker/swarm/deploy.sh
+++ docker/swarm/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-# The "--resolve-image never" option is needed because `docker info` and
+# The "--resolve-image never" option is needed if `docker info` and
 # `docker inspect` on the container report different results.
 # See: https://github.com/docker/swarmkit/issues/2294 for more details
 # This was an issue using https://hub.docker.com/r/resin/rpi-raspbian/ but
 # using arm32v7/debian:stretch seems to be fine
-docker stack deploy -c docker-compose.yml --resolve-image never app
+docker stack deploy -c docker-compose.yml app
