8acc6f72224e722410bc8edc80e37e67b9036046
Matt Knight <matthew.knight@gmail.com>
2018-11-01 18:03:42 -0600

    Adding InfluxDB

--- docker/swarm/build.sh
+++ docker/swarm/build.sh
@@ -3,6 +3,8 @@
 REGISTRY_URL=localhost:5000
 TAG=latest
 
+docker build -t $REGISTRY_URL/influxdb:$TAG influxdb && docker push $REGISTRY_URL/influxdb:$TAG
+
 docker build -t $REGISTRY_URL/mosquitto:$TAG mosquitto && docker push $REGISTRY_URL/mosquitto:$TAG
 
 docker build -t $REGISTRY_URL/node-red:$TAG node-red && docker push $REGISTRY_URL/node-red:$TAG
